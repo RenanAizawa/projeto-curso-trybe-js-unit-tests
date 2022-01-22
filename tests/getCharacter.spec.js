c04ead03dba651034f3e3d9b8f41e328e3c822f8
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-01-22 13:44:08 -0500

    Finalizando Requisitos da questao 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -66,6 +66,11 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
       phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
     });
     // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('ARYA')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
     expect(getCharacter('Renan')).toBeUndefined();
   });
